a9f3f2bb3c90eb338cd84a589409968364c312f6
PANERO72 <panerootero@gmail.com>
2023-01-03 00:08:05 +0100

    Primera confirmación

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,18 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+
+body {
+    font-family: 'Ubuntu', sans-serif !important;
+    background-color: #f0fdf0 !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import './styles.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+//import { createPopper } from '@popperjs/core'
+
+// const popcorn = document.querySelector('#popcorn');
+// const tooltip = document.querySelector('#tooltip');
+// createPopper(popcorn, tooltip, {
+//   placement: 'top-end',
+// });
+
+createApp(App).mount('#app');
